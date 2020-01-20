c9e0e78a07b6f30146bec5b5ddc24408446ea1be
valentinchelle <valentin.chelle@hotmail.fr>
2020-01-20 12:51:43 -0800

    Added Model General Functions

--- users/models/User.js
+++ users/models/User.js
@@ -42,4 +42,67 @@ const UserSchema = new Schema({
   }
 });
 
+const Model = mongoose.model("Users", userSchema);
+
+// General Function for a model
+exports.findByEmail = email => {
+  return Model.find({ email: email });
+};
+
+exports.findById = id => {
+  return Model.findById(id).then(result => {
+    result = result.toJSON();
+    delete result._id;
+    delete result.__v;
+    return result;
+  });
+};
+
+exports.create = userData => {
+  const user = new Model(userData);
+  return user.save();
+};
+
+exports.list = (perPage, page) => {
+  return new Promise((resolve, reject) => {
+    Model.find()
+      .limit(perPage)
+      .skip(perPage * page)
+      .exec(function(err, users) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(users);
+        }
+      });
+  });
+};
+
+exports.patch = (id, userData) => {
+  return new Promise((resolve, reject) => {
+    Model.findById(id, function(err, user) {
+      if (err) reject(err);
+      for (let i in userData) {
+        user[i] = userData[i];
+      }
+      user.save(function(err, updatedUser) {
+        if (err) return reject(err);
+        resolve(updatedUser);
+      });
+    });
+  });
+};
+
+exports.removeById = userId => {
+  return new Promise((resolve, reject) => {
+    Model.remove({ _id: userId }, err => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(err);
+      }
+    });
+  });
+};
+
 module.exports = User = mongoose.model("users", UserSchema);
